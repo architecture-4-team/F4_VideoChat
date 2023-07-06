f06e5a929e306dc8bd37c3eff0fdd051f344f7c4
Yongsung Kang <yongsung.kang@gmail.com>
2023-07-06 17:51:31 +0900

    initial commit

---
+++ F4Main.cpp
@@ -0,0 +1,227 @@
+#include <Windows.h>
+#include <tchar.h>
+#include <string>
+#include <wrl.h>
+#include <wil/com.h>
+// <IncludeHeader>
+// include WebView2 header
+#include "WebView2.h"
+// </IncludeHeader>
+// 
+
+using namespace Microsoft::WRL;
+// Pointer to WebViewController
+static wil::com_ptr<ICoreWebView2Controller> webviewController;
+
+// Pointer to WebView window
+static wil::com_ptr<ICoreWebView2> webview;
+
+// Global variables
+HINSTANCE g_hInstance;
+
+// Forward declarations
+LRESULT CALLBACK MainWndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK ChildWndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	HWND hMainWindow;
+	HWND hChildWindow;
+
+	// Store the instance handle
+	g_hInstance = hInstance;
+
+	// Register the main window class
+	WNDCLASSEX wcex = { sizeof(WNDCLASSEX), CS_HREDRAW | CS_VREDRAW, MainWndProc, 0, 0, hInstance, nullptr,
+						LoadCursor(nullptr, IDC_ARROW), nullptr, nullptr, _T("MainWindowClass"), nullptr };
+	if (!RegisterClassEx(&wcex))
+	{
+		MessageBox(nullptr, _T("Failed to register window class."), _T("Error"), MB_ICONERROR | MB_OK);
+		return 1;
+	}
+
+	// Register the child window class
+	WNDCLASSEX wcexChild = { sizeof(WNDCLASSEX), CS_HREDRAW | CS_VREDRAW, ChildWndProc, 0, 0, hInstance, nullptr,
+							 LoadCursor(nullptr, IDC_ARROW), nullptr, nullptr, _T("ChildWindowClass"), nullptr };
+	if (!RegisterClassEx(&wcexChild))
+	{
+		MessageBox(nullptr, _T("Failed to register child window class."), _T("Error"), MB_ICONERROR | MB_OK);
+		return 1;
+	}
+
+	// Create the main window
+	hMainWindow = CreateWindowEx(0, _T("MainWindowClass"), _T("Main Window"), WS_OVERLAPPEDWINDOW,
+		0, 0, 0, 0, nullptr, nullptr, hInstance, nullptr);
+	
+	if (!hMainWindow)
+	{
+		MessageBox(nullptr, _T("Failed to create main window."), _T("Error"), MB_ICONERROR | MB_OK);
+		return 1;
+	}
+
+	// Create a button
+	HWND hButton = CreateWindowEx(0, _T("BUTTON"), _T("Contact List"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+		10, 10, 100, 30, hMainWindow, reinterpret_cast<HMENU>(1), hInstance, nullptr);
+	if (!hButton)
+	{
+		MessageBox(nullptr, _T("Failed to create button."), _T("Error"), MB_ICONERROR | MB_OK);
+		return 1;
+	}
+
+	// Create the child window
+	hChildWindow = CreateWindowEx(0, _T("ChildWindowClass"), _T("Login Window"), WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, 600, 600, hMainWindow, nullptr, hInstance, nullptr);
+
+	if (!hChildWindow)
+	{
+		MessageBox(hMainWindow, _T("Failed to create child window."), _T("Error"), MB_ICONERROR | MB_OK);
+		return 1;
+	}
+
+	// Show and update the main window
+	ShowWindow(hMainWindow, SW_MAXIMIZE);
+	UpdateWindow(hMainWindow);
+
+	// Show and update the main window
+	ShowWindow(hChildWindow, nCmdShow);
+	UpdateWindow(hChildWindow);
+
+	CreateCoreWebView2EnvironmentWithOptions(nullptr, nullptr, nullptr,
+		Callback<ICoreWebView2CreateCoreWebView2EnvironmentCompletedHandler>(
+			[hChildWindow](HRESULT result, ICoreWebView2Environment* env) -> HRESULT {
+
+				// Create a CoreWebView2Controller and get the associated CoreWebView2 whose parent is the main window hWnd
+				env->CreateCoreWebView2Controller(hChildWindow, Callback<ICoreWebView2CreateCoreWebView2ControllerCompletedHandler>(
+					[hChildWindow](HRESULT result, ICoreWebView2Controller* controller) -> HRESULT {
+						if (controller != nullptr) {
+							webviewController = controller;
+							webviewController->get_CoreWebView2(&webview);
+						}
+
+						// Add a few settings for the webview
+						// The demo step is redundant since the values are the default settings
+						wil::com_ptr<ICoreWebView2Settings> settings;
+						webview->get_Settings(&settings);
+						settings->put_IsScriptEnabled(TRUE);
+						settings->put_AreDefaultScriptDialogsEnabled(TRUE);
+						settings->put_IsWebMessageEnabled(TRUE);
+
+						// Resize WebView to fit the bounds of the parent window
+						RECT bounds;
+						GetClientRect(hChildWindow, &bounds);
+						webviewController->put_Bounds(bounds);
+
+						// Schedule an async task to navigate to Bing
+						webview->Navigate(L"http://www.google.com");
+
+						EventRegistrationToken token;
+						// <NavigationEvents>
+						// Step 4 - Navigation events
+						// register an ICoreWebView2NavigationStartingEventHandler to cancel any non-https navigation
+/*
+						webview->add_NavigationStarting(Callback<ICoreWebView2NavigationStartingEventHandler>(
+							[](ICoreWebView2* webview, ICoreWebView2NavigationStartingEventArgs* args) -> HRESULT {
+								wil::unique_cotaskmem_string uri;
+								args->get_Uri(&uri);
+								std::wstring source(uri.get());
+								if (source.substr(0, 5) != L"https") {
+									args->put_Cancel(true);
+								}
+								return S_OK;
+							}).Get(), &token);
+*/
+						// </NavigationEvents>
+
+						// <Scripting>
+						// Step 5 - Scripting
+						// Schedule an async task to add initialization script that freezes the Object object
+						webview->AddScriptToExecuteOnDocumentCreated(L"Object.freeze(Object);", nullptr);
+						// Schedule an async task to get the document URL
+						webview->ExecuteScript(L"window.document.URL;", Callback<ICoreWebView2ExecuteScriptCompletedHandler>(
+							[](HRESULT errorCode, LPCWSTR resultObjectAsJson) -> HRESULT {
+								LPCWSTR URL = resultObjectAsJson;
+								//doSomethingWithURL(URL);
+								return S_OK;
+							}).Get());
+						// </Scripting>
+
+						// <CommunicationHostWeb>
+						// Step 6 - Communication between host and web content
+						// Set an event handler for the host to return received message back to the web content
+						webview->add_WebMessageReceived(Callback<ICoreWebView2WebMessageReceivedEventHandler>(
+							[](ICoreWebView2* webview, ICoreWebView2WebMessageReceivedEventArgs* args) -> HRESULT {
+								wil::unique_cotaskmem_string message;
+								args->TryGetWebMessageAsString(&message);
+								// processMessage(&message);
+								webview->PostWebMessageAsString(message.get());
+								return S_OK;
+							}).Get(), &token);
+
+						// Schedule an async task to add initialization script that
+						// 1) Add an listener to print message from the host
+						// 2) Post document URL to the host
+						webview->AddScriptToExecuteOnDocumentCreated(
+							L"window.chrome.webview.addEventListener(\'message\', event => alert(event.data));" \
+							//L"window.chrome.webview.postMessage(window.document.URL);",
+							L"window.chrome.webview.postMessage(`hello?`);",
+							nullptr);
+						// </CommunicationHostWeb>
+
+						return S_OK;
+					}).Get());
+				return S_OK;
+			}).Get());
+
+	// Message loop
+	MSG msg;
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return static_cast<int>(msg.wParam);
+}
+
+LRESULT CALLBACK MainWndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	case WM_COMMAND:
+		if (LOWORD(wParam) == 1) 
+		{
+			HWND hChildWnd2 = CreateWindowEx(0, _T("ChildWindowClass"), _T("Contact List"), WS_OVERLAPPEDWINDOW,
+				CW_USEDEFAULT, CW_USEDEFAULT, 400, 300, nullptr, nullptr, g_hInstance, nullptr);
+			if (!hChildWnd2)
+			{
+				MessageBox(hWnd, _T("Failed to create child window."), _T("Error"), MB_ICONERROR | MB_OK);
+				return 1;
+			}
+
+			// Show the child window
+			ShowWindow(hChildWnd2, SW_SHOW);
+			UpdateWindow(hChildWnd2);
+		}
+		break;
+	}
+
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+LRESULT CALLBACK ChildWndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_CLOSE:
+		DestroyWindow(hWnd);
+		break;
+
+	case WM_DESTROY:
+		break;
+	}
+
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
---
+++ MultiMediaReceiver.h
@@ -0,0 +1,51 @@
+#pragma once
+#ifndef MULTIMEDIARECEIVER_H
+#define MULTIMEDIARECEIVER_H
+
+#include <gst/gst.h>
+#include <iostream>
+#include "MultimediaInterface.h"
+
+class MultimediaReceiver : public MultimediaInterface {
+public:
+    MultimediaReceiver();
+    ~MultimediaReceiver();
+    bool initialize();
+    void cleanup();
+    void start();
+    void stop();
+    void setPort(int videoPort, int audioPort);
+    void setJitterBuffer(int latency);
+    void setRTP();
+    void setWindow(void* hVideo);
+private:
+    // Pipeline
+    GstElement* receiverVideoPipeline;
+    GstElement* receiverAudioPipeline;
+
+    GstElement* videoSrc;
+    GstElement* videoCapsfilter;
+    GstElement* jitterbufferVideo;
+    GstElement* videoDepay;
+    GstElement* videoDec;
+    GstElement* videoSink;
+
+    GstElement* audioSrc;
+    GstElement* audioCapsfilter;
+    GstElement* jitterbufferAudio;
+    GstElement* audioDec;
+    GstElement* audioDepay;
+    GstElement* audioConv;
+    GstElement* audioSink;
+
+    GstBus* receiverVideoBus;
+    GstBus* receiverAudioBus;
+
+    GMainLoop* receiverLoop;
+};
+static gboolean handle_receiver_video_bus_message(GstBus* bus, GstMessage* msg, gpointer data);
+static gboolean handle_receiver_audio_bus_message(GstBus* bus, GstMessage* msg, gpointer data);
+
+static GstPadProbeReturn probe_callback(GstPad* pad, GstPadProbeInfo* info, gpointer user_data);
+
+#endif  // MULTIMEDIARECEIVER_H
